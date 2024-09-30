bae27dc0ed452187ef2b3a8f0bf9290f4d0b2f69
Satyam Singh <businesswithsatyam9555@gmail.com>
2024-09-30 15:21:18 +0530

    URL changed for admin

--- rooms-app-admin/src/app/config/axios.ts
+++ rooms-app-admin/src/app/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001',  // Adjust according to your backend URL
+  baseURL: 'http://localhost:5000',  // Adjust according to your backend URL
 });
 
 // Interceptor to add the Authorization header with Bearer token for all requests
